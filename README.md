6b58b2d710d444946cb27c7394b39250092843c1
Robbie Campbell <robbie.campbell@postcodelottery.co.uk>
2021-08-26 11:19:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# city_distance
---
+++ city_distance.py
@@ -0,0 +1,26 @@
+'''
+A script which asks the user for their unit preferance then determines the distance between Tallinn and Helsinki
+'''
+
+from geopy import distance
+
+def cities():
+
+    print("This program will determine the distance between Tallinn and Helsinki")
+    unit = input("Please select your unit of measurement: Miles or Kilometres: ")
+
+    tallinn = (59.4370, 24.7536) # the long and lat for each city
+    helsinki = (60.1699, 24.9384)
+
+    if unit.lower() == 'miles' or unit.lower() == 'm': # checks whether user has selected miles or kilometeres
+        # uses geopy's distance method to find the distance in miles between two tuples of co-ordinates, and round to set it to
+        # two decimal place precision
+        print("You have selected Miles")
+        print(f"The distance between Tallinn and Helsinki is {round(distance.distance(tallinn, helsinki).miles,2)} miles") 
+    else:
+        # same as above but for kilometres
+        print("You have selected Kilometres")
+        print(f"The distance between Tallinn and Helsinki is {round(distance.distance(tallinn,helsinki).km,2)} km")
+
+
+cities()
